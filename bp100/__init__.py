2101379c5127bc1d4afcac62ee3734ff0530eb69
Christopher H. Casebeer <christopher@chc.name>
2026-03-20 21:55:58 -0400

    Import BLE UUID Enums directly into package

--- bp100/__init__.py
+++ bp100/__init__.py
@@ -4,3 +4,4 @@ from . import util
 
 from .TranstekController import TranstekController
 from .TranstekBleDriver import TranstekBleDriver
+from .bleUuids import ( GattServices, DeviceInfoCharacteristics, TranstekCharacteristics)
